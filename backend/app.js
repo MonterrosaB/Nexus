1f147b9d992a5617f9c857833ccd9a6c29a92933
Rodrigo Monterrosa <20230197@ricaldone.edu.sv>
2025-03-21 10:27:26 -0600

    App Done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 import express from "express";
+
 import productRoutes from "./src/routes/product.js";
+import orderRoutes from "./src/routes/order.js";
+import categoryRoutes from "./src/routes/category.js";
+import providerRoutes from "./src/routes/provider.js";
+import userRoutes from "./src/routes/user.js";
+import brandRoutes from "./src/routes/brand.js";
+import cartProductsRoutes from "./src/routes/cartProduct.js";
+import customerRoutes from "./src/routes/customer.js";
 
 const app = express();
 
@@ -9,11 +17,9 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/providers", providerRoutes);
-
-
-app.use("/api/users", productUser);
-app.use("/api/brands", productBrand);
-app.use("/api/cartProducts", productCartProduct);
-app.use("/api/customers", productCustomer);
+app.use("/api/users", userRoutes);
+app.use("/api/brands", brandRoutes);
+app.use("/api/cartProducts", cartProductsRoutes);
+app.use("/api/customers", customerRoutes);
 
 export default app;

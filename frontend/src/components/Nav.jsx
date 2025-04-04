a8570e14ab1ea34aa6c5e4deec20df227b16152b
MonterrosaB <rodrigo.monterrosa@hotmail.com>
2025-04-04 14:47:19 -0600

    components folder

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Nav = () =>{
+    return (
+        <header className="bg-black">
+            <div className="">
+
+            </div>
+            <nav className="">
+
+            </nav>
+        </header>
+    )
+}

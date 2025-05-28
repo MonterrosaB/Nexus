e1f4ae5f54b196ccb77881907e702064dde0a76e
MonterrosaB <rodrigo.monterrosa@hotmail.com>
2025-05-27 20:41:47 -0600

    Insert Providers Done and Input Fixed

--- frontend/src/pages/admin/AgregarProveedores/AgregarProveedores.jsx
+++ frontend/src/pages/admin/AgregarProveedores/AgregarProveedores.jsx
@@ -2,9 +2,33 @@ import Reminder from "../../../components/Reminder";
 import Button from "../../../components/Button";
 import Input from "../../../components/Input";
 import Image from "../../../assets/proveedores.webp"
+import useDataProviders from "../../../components/Proveedores/useDataProveedores"
 
 
 const AgregarProveedores = () => {
+
+    const {
+    providers,
+        setProviders,
+        loading,
+        setLoading,
+        providerName,
+         setProviderName,
+        providerLastName,
+         setProviderLastName,
+        providerCompany,
+        setProviderCompany,
+        providerEmail, 
+        setproviderEmail,
+        providerPhoneNumber, 
+        setProviderPhoneNumber,
+        id,
+        setId,
+        saveProvider,
+        deleteProduct,
+        updateProduct,
+        handleEdit,
+  } = useDataProviders();
 
     const data = {
         first: "Recuerda ingresar el Nombre completo del proveedor y la compañía.",
@@ -29,24 +53,42 @@ const AgregarProveedores = () => {
                         <img src={Image} alt="" className="w-sm" />
                     <div>
                         <h2 className="font-bold text-3xl text-[#2B3674] pb-3">Proveedor</h2>
-                        <Input
+                        <div className="flex justify-center gap-2">
+                            <Input
                             label={"Nombre del proveedor"}
-                            id={"proveedor"}
-                            type={"text"} />
+                            id={"nombreProveedor"}
+                            type={"text"}
+                            onChange={(e) => setProviderName(e.target.value)}
+                            value={providerName} />
+                            <Input
+                            label={"Apellido del proveedor"}
+                            id={"apellidoProveedor"}
+                            type={"text"}
+                            onChange={(e) => setProviderLastName(e.target.value)}
+                            value={providerLastName} />
+                        </div>
+                        
                         <Input
                             label={"Compañía"}
                             id={"compañia"}
-                            type={"text"} />
+                            type={"text"}
+                            onChange={(e) => setProviderCompany(e.target.value)}
+                            value={providerCompany} />
                         <Input
                             label={"Correo Electronico"}
                             id={"email"}
-                            type={"email"} />
+                            type={"email"}
+                            onChange={(e) => setproviderEmail(e.target.value)}
+                            value={providerEmail} />
                         <Input
                             label={"Número de teléfono"}
                             id={"telefono"}
-                            type={"string"} />
+                            type={"string"}
+                            onChange={(e) => setProviderPhoneNumber(e.target.value)}
+                            value={providerPhoneNumber} />
                         <Button
                             text={"Agregar proveedor"}
+                            onClick={saveProvider}
                         />
                     </div>
                 </form>
--- frontend/src/pages/admin/TablasDatos/TablasDatos.jsx
+++ frontend/src/pages/admin/TablasDatos/TablasDatos.jsx
@@ -1,5 +1,37 @@
+import { Pencil, Trash } from "lucide-react"
 const TablasDatos = () => {
-  return <div>Hola desde Cambiar Cuenta</div>;
+  return (
+
+<div className="p-6 bg-[#F4F7FE] min-h-dvh flex flex-col gap-8">
+    <div class="relative overflow-x-auto sm:rounded-lg p-6">
+  <h2 className="p-4">Lista de Usuarios</h2>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs uppercase text-black">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white">
+                <th scope="row" class="px-6 py-4 font-medium text-[#333]">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="flex gap-4 px-6 py-4 text-[#2600FE]">
+                    <Trash/>
+                    <Pencil/>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</div>
+
+  )
 };
 
 export default TablasDatos;
